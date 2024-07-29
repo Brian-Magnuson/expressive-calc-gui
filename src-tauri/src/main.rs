97492be847b0237691212dc19fbc88b935742574
Brian-Magnuson <92892499+Brian-Magnuson@users.noreply.github.com>
2024-07-29 11:30:55 -0400

    Remove core greet command

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,12 +5,6 @@ use expressive_calc::Calculator;
 use std::sync::Mutex;
 use tauri::Manager;
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
-
 #[derive(serde::Deserialize)]
 struct StringPayload {
     value: String,
@@ -41,7 +35,6 @@ fn main() {
 
             Ok(())
         })
-        .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
