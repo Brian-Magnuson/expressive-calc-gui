f7dd92caa610a482137d87d96f74febb70c2b917
Brian-Magnuson <92892499+Brian-Magnuson@users.noreply.github.com>
2024-07-29 11:29:40 -0400

    Update core event handlers

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,9 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::sync::Mutex;
-
 use expressive_calc::Calculator;
+use std::sync::Mutex;
 use tauri::Manager;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
@@ -13,24 +12,31 @@ fn greet(name: &str) -> String {
 }
 
 #[derive(serde::Deserialize)]
-struct ExpressionPayload {
-    expression: String,
+struct StringPayload {
+    value: String,
 }
 
 fn main() {
     tauri::Builder::default()
         .setup(|app| {
+            let window = app.get_window("main").unwrap();
             let calculator = Mutex::new(Calculator::new());
 
             app.listen_global("evaluate", move |event| {
-                let expr = event.payload().unwrap();
-                let payload: ExpressionPayload = serde_json::from_str(expr).unwrap();
-                let result = match calculator.lock().unwrap().evaluate(&payload.expression) {
+                // The event message is a JSON string
+                let message = event.payload().unwrap();
+                // Deserialize the JSON string into a struct
+                let payload: StringPayload = serde_json::from_str(&message).unwrap();
+
+                // Lock the calculator and evaluate the expression
+                let result = match calculator.lock().unwrap().evaluate(&payload.value) {
                     Ok((name, value)) => format!("{} = {}", name, value),
                     Err(err) => err.to_string(),
                 };
+                // Calculator lock is released here
 
-                println!("Result: {}", result);
+                // Send the result back to the front-end
+                window.emit("result", result).unwrap();
             });
 
             Ok(())
