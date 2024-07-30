53d77a1a7feac6eb88226e77f0b5258ccac02347
Brian-Magnuson <92892499+Brian-Magnuson@users.noreply.github.com>
2024-07-30 10:57:21 -0400

    Add basic styling to calculator

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,29 @@
 <script lang="ts">
+  import { onMount } from "svelte";
   import { invoke } from "@tauri-apps/api/tauri";
 
+  let inputElement: HTMLInputElement;
+
+  onMount(() => {
+    inputElement.focus();
+  });
+
   let input = "";
-  let output = "";
+  let outputs: string[] = [];
 
   async function handleSubmit(_: SubmitEvent) {
-    output = (await invoke("evaluate_expression", {
+    const output = (await invoke("evaluate_expression", {
       expression: input,
     })) as string;
+
+    input = "";
+    outputs = [`${output}`, ...outputs];
   }
 
   async function handleClear() {
     input = "";
-    output = "";
-    await invoke("clear_expression");
+    outputs = [];
+    await invoke("reset_calculator");
   }
 
   async function handleClearEntry() {
@@ -22,13 +32,22 @@
 </script>
 
 <main>
-  <form on:submit|preventDefault={handleSubmit}>
-    <input type="text" bind:value={input} />
+  <div class="upper">
+    {#each outputs as output}
+      <div class="entry">{output}</div>
+    {/each}
+  </div>
+  <form class="lower" on:submit|preventDefault={handleSubmit}>
+    <input
+      class="input"
+      type="text"
+      bind:this={inputElement}
+      bind:value={input}
+    />
     <button type="submit"> &equals; </button>
     <button type="button" on:click={handleClear}> C </button>
     <button type="button" on:click={handleClearEntry}> CE </button>
   </form>
-  <div>{output}</div>
 </main>
 
 <style>
@@ -46,6 +65,51 @@
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     -webkit-text-size-adjust: 100%;
+  }
+
+  main {
+    /* align */
+    height: 100vh;
+    width: 100vw;
+    /* style */
+    /* background: red; */
+    /* content */
+    display: flex;
+    flex-direction: column;
+  }
+
+  .upper {
+    /* align */
+    flex: 1;
+    /* style */
+    /* background: blue; */
+    /* content */
+    display: flex;
+    flex-direction: column-reverse;
+  }
+
+  .upper .entry {
+    /* style */
+    text-align: right;
+    border-top: 1px solid #ccc;
+    /* content */
+    padding: 0.5rem;
+  }
+
+  .lower {
+    /* align */
+    flex: 1;
+  }
+
+  .lower .input {
+    /* align */
+    display: block;
+    box-sizing: border-box;
+    width: 100%;
+    /* style */
+    border-radius: 0;
+    /* content */
+    padding: 0.5rem;
   }
 
   @media (prefers-color-scheme: dark) {
